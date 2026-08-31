f3019919c0f679e1ed7120c9494a9a2f3d2c537c
khushi70544 <agnihotrimayank99@gmail.com>
2026-08-31 11:15:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from telebot import TeleBot, types
 
 # Bot Token
 BOT_TOKEN = "8888661139:AAFfUeYVLG8kwQ4tDp1TWs_a9wWmqEyVuGQ" # Apna bot token yahan dalein
-bot = TeleBot(8888661139:AAFfUeYVLG8kwQ4tDp1TWs_a9wWmqEyVuGQ)
+bot = TeleBot(BOT_TOKEN)
+
 
 # Dynamic Inventory Data - Updated Prices & Plans
 inventory = {
